4044b32efb42e34c552d0be05b21258fea1832b3
Michael D. Hoyle <hoylemd@gmail.com>
2015-10-27 19:49:12 -0400

    move some of the functionality into a class

--- primes.py
+++ primes.py
@@ -7,25 +7,6 @@ def test_divisible(numerator, divisor):
     return (numerator % divisor) == 0
 
 
-def test_for_prime_divisor(t):
-    if t > 99999:
-        raise Exception("going way too far!")
-
-    left = 0
-    right = len(primes) - 1
-
-    while left <= right:
-        if test_divisible(t, primes[left]):
-            return True
-        left += 1
-
-        if test_divisible(t, primes[right]):
-            return True
-        right -= 1
-
-    return False
-
-
 def binary_search(t, array):
     left = 0
     right = len(array) - 1
@@ -42,23 +23,49 @@ def binary_search(t, array):
     return False
 
 
-def test_prime(t):
-    greatest = primes[-1]
-    # if the test number is higher than the highest known prime,
-    # calculate primes until we have enough
-    if t > greatest:
-        next_candidate = greatest
-        while t > greatest:
-            # start calculating primes until we reach this one or go higher
-            next_candidate += 1
-            if not test_for_prime_divisor(next_candidate):
-                primes.append(next_candidate)
-                greatest = next_candidate
-        if t == greatest:
-            return True
+class PrimeSage(object):
+    def __init__(self, data_path):
+        self.data_path = data_path
+
+        self.primes = [2]
+
+    def is_prime(self, sample):
+        greatest = self.primes[-1]
+        # if the test number is higher than the highest known prime,
+        # calculate primes until we have enough
+        if sample > greatest:
+            next_candidate = greatest
+            while sample > greatest:
+                # start calculating primes until we reach this one or go higher
+                next_candidate += 1
+                if not self.has_prime_divisor(next_candidate):
+                    self.primes.append(next_candidate)
+                    greatest = next_candidate
+            if sample == greatest:
+                return True
+            return False
+        return binary_search(sample, self.primes)
+
+    def has_prime_divisor(self, sample):
+        if sample > (self.primes[-1] * 2):
+            raise Exception("going way too far!")
+
+        left = 0
+        right = len(self.primes) - 1
+
+        while left <= right:
+            if test_divisible(sample, self.primes[left]):
+                return True
+            left += 1
+
+            if test_divisible(sample, self.primes[right]):
+                return True
+            right -= 1
+
         return False
-    return binary_search(t, primes)
+
+sage = PrimeSage('primes.pickle')
 
 
 def verboseTestPrime(t):
-    print "%d is%s prime." % (t, ('' if test_prime(t) else " not"))
+    print "%d is%s prime." % (t, ('' if sage.is_prime(t) else " not"))
